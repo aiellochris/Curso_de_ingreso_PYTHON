ac8704f26af5f47fe28cfaa674326b0a184981d6
aiellochris <aiello.sopranista@gmail.com>
2024-02-05 19:18:15 -0300

    Ejercicio 4 entradas y salidas resuelto

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:Cristian
+apellido:Aiello
 ---
 Ejercicio: entrada_salida_04
 ---
@@ -33,7 +33,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre_alumno = prompt("Ingreso","Ingrese nombre")
+        self.txt_nombre.delete(0, "end")
+        self.txt_nombre.insert(0, nombre_alumno)
         
     
 if __name__ == "__main__":
